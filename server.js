fcffdd5749d98188e774514a1f2f455447fb5871
MorcilloA <alexy.morcillo@gmail.com>
2021-08-28 17:01:19 +0200

    no more fixed port for heroku

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const path = require("path");
 
 const app = express();
 app.use(express.static("static"));
-const port = 8000;
+const port = process.env.PORT || 8000;
 
 app.get('/', function(req, res) {
     res.sendFile(path.join(__dirname, '/index.html'));
